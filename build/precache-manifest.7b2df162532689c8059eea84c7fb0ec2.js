1ce067e6865ff21d46fab632cc650e883412b645
Valentyn <valentyn28101993@ukr.net>
2020-05-10 21:13:12 +0300

    raw slider 1st

---
+++ build/precache-manifest.7b2df162532689c8059eea84c7fb0ec2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20340edf7b34e7f3c07383c78e85f55",
+    "url": "/slider-1-react-slick/index.html"
+  },
+  {
+    "revision": "2152b1d2da36eae41586",
+    "url": "/slider-1-react-slick/static/css/2.d3c02882.chunk.css"
+  },
+  {
+    "revision": "2829324d5a93976f2f16",
+    "url": "/slider-1-react-slick/static/css/main.7e69bcb2.chunk.css"
+  },
+  {
+    "revision": "2152b1d2da36eae41586",
+    "url": "/slider-1-react-slick/static/js/2.09f53240.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/slider-1-react-slick/static/js/2.09f53240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2829324d5a93976f2f16",
+    "url": "/slider-1-react-slick/static/js/main.2af7fa1b.chunk.js"
+  },
+  {
+    "revision": "a07e0dfd882ca0927c3f",
+    "url": "/slider-1-react-slick/static/js/runtime-main.89581e39.js"
+  },
+  {
+    "revision": "0dbb617f55764e002159d80502d765d0",
+    "url": "/slider-1-react-slick/static/media/car-mazda3.0dbb617f.jpg"
+  },
+  {
+    "revision": "8e2a5f0586ea6faa0525b3de464088a5",
+    "url": "/slider-1-react-slick/static/media/car-new-scoda.8e2a5f05.jpg"
+  },
+  {
+    "revision": "e5be3f13a223a3088389911cb86057c5",
+    "url": "/slider-1-react-slick/static/media/car-scoda-fabia.e5be3f13.jpg"
+  },
+  {
+    "revision": "47948a71878be9021c42ff630f4245bf",
+    "url": "/slider-1-react-slick/static/media/car-shevrole.47948a71.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/slider-1-react-slick/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/slider-1-react-slick/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/slider-1-react-slick/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/slider-1-react-slick/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MySlider from "../MySlider/MySlider";
+import styles from './App.module.css'
+
+
+function App() {
+  return (
+    <div className={styles.appWraper}>
+      <MySlider />
+    </div>
+  );
+}
+
+export default App;
